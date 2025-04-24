3a0d41c83deda1c351bd828605168cb783fd1060
Eduardo Hernández <jhernandezg1403@gmail.com>
2025-04-23 21:46:11 -0600

    añade ejercicios de comprensión de listas

--- Modelado de datos con Python/Python para DataSciences/funciones, estructuras de datos y excepciones/Funciones.py
+++ Modelado de datos con Python/Python para DataSciences/funciones, estructuras de datos y excepciones/Funciones.py
@@ -230,8 +230,3 @@ paseo = gasto_paseo()
 hotel = gasto_hotel()
 print(f'Con base en los gastos definidos, un viaje de {dias} días a {ciudad} desde Recife costaría {hotel + paseo + gasolina: .2f} reales.')
 
-'''10 - Has comenzado una pasantía en una empresa que trabaja con procesamiento de lenguaje natural (NLP). Tu líder te solicitó que crees un fragmento de código que reciba una frase escrita por el usuario y filtre solo las palabras con un tamaño mayor o igual a 5, mostrándolas en una lista. Esta demanda se centra en el análisis del patrón de comportamiento de las personas al escribir palabras de esta cantidad de caracteres o más.
-
-Consejo: utiliza las funciones lambda y filter() para filtrar estas palabras. Recordando que la función integrada filter() recibe una función (en nuestro caso, una función lambda) y filtra un iterable según la función. Para tratar la frase, utiliza replace() para cambiar ',' '.', '!' y '?' por espacio.
-
-Utiliza la frase "Aprender Python aquí en Alura es muy bueno" para probar el código.'''
--- Modelado de datos con Python/Python para DataSciences/funciones, estructuras de datos y excepciones/tempCodeRunnerFile.py
+++ Modelado de datos con Python/Python para DataSciences/funciones, estructuras de datos y excepciones/tempCodeRunnerFile.py
@@ -1,45 +1,6 @@
-gastos_paseo = [200, 400, 250, 300]
-hospedaje = 150
-rendimiento = 14
-costo_gasolina = 5
-distancia = [850, 800, 300, 550]
-ciudades = ['Salvador', 'Fortaleza', 'Natal',  'Aracaju']
+glicemia = [129, 82, 60, 97, 101, 65, 62, 167, 87, 53, 58, 92, 66, 120, 109, 62, 86, 96, 103, 88, 155, 52, 89, 73]
+rangos = ['Hipoglicemia','Normal','Alterada','Diabetes']
 
-ciudad = input('¿A qué ciudad quieres ir? (Salvador, Fortaleza, Natal y Aracaju): ')
+rangos_glicemia = [(rangos[i], glicemia[i]) for i in range(len(glicemia))]
 
-ciudad = ciudad.capitalize()
-
-while ciudad not in ciudades:
-    print('Opción no válida, por favor escribe alguna de las opciones')
-    ciudad = input('¿A qué ciudad quieres ir? (Salvador, Fortaleza, Natal y Aracaju): ')
-
-    ciudad = ciudad.capitalize()
-
-dias = int(input('¿Cuántos días serán? '))
-
-print(ciudad)
-#Gasto hotel
-def gasto_hotel():
-    hotel = dias * hospedaje
-    print(f'Pago hotel es de {hotel}')
-    return hotel
-
-#Gasto gasolina
-def gasto_gasolina():
-    indice = ciudades.index(ciudad)
-    trayecto = distancia[indice] * 2
-    pago_gasolina = (trayecto / rendimiento) * costo_gasolina
-    print(f'La distancia es {trayecto} y costará {pago_gasolina}')
-    return pago_gasolina
-
-#Gasto paseo (paseo y alimentacion)
-def gasto_paseo():
-    indice = ciudades.index(ciudad)
-    pago_paseo = gastos_paseo[indice] * dias
-    print(f'El paseo costará {pago_paseo}')
-    return pago_paseo
-
-gasolina = gasto_gasolina()
-paseo = gasto_paseo()
-hotel = gasto_hotel()
-print(f'Con base en los gastos definidos, un viaje de {dias} días a {ciudad} desde Recife costaría {hotel + paseo + gasolina: .2f} reales.')
+print(rangos_glicemia)
